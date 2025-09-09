b0c8b46b245986835c94414cf5ca07b2c76adcbd
Meghana Sai Vattikuti <meghanasai1802@gmail.com>
2025-09-08 20:17:02 -0400

    Prod push infra changes

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -6,6 +6,7 @@ module "networking" {
   private_subnet_count = 2
 }
 #Testing changes
+#infra changes
 module "security" {
   source  = "../../modules/security"
   vpc_id  = module.networking.vpc_id
