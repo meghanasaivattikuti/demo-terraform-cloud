5e1ccdaee386e31223a655b9cd2315ad405a2090
Meghana Sai Vattikuti <meghanasai1802@gmail.com>
2025-09-10 14:50:21 -0400

    Policy testing

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources in"
   type        = string
-  default     = "us-east-1"
+  default     = "us-weseastt-1"
 }
 
 variable "ami_id" {
-  description = "AMI to use for the EC2 instance"
+  description = "AMI to use for the EC2 instances"
   type        = string
 }
 
