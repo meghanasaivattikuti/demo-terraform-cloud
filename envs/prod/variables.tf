1ff297c34aecf413294a57dd2f0a6926c04ea8cc
Meghana Sai Vattikuti <meghanasai1802@gmail.com>
2025-09-10 15:14:10 -0400

    Demo runs

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -12,7 +12,7 @@ variable "ami_id" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.large"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
